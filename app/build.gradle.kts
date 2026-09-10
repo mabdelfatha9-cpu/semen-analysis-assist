b824431f360af912b843e01f2bfbabf2e1905a13
mabdelfatha9-cpu <mabdelfatha9@gmail.com>
2026-09-11 02:25:05 +0300

    Version 0.5.0 self-learning feedback upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.labtools.semenanalysis"
         minSdk = 26
         targetSdk = 34
-        versionCode = 4
-        versionName = "0.4.0-monochrome"
+        versionCode = 5
+        versionName = "0.5.0-learning"
 
         buildConfigField(
             "String",
