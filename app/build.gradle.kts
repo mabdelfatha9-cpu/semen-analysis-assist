e1049f563ded861a0038fc63f0de10bb7b400057
mabdelfatha9-cpu <mabdelfatha9@gmail.com>
2026-09-11 02:03:51 +0300

    Version 0.4.0 morphology + PDF export

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.labtools.semenanalysis"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
-        versionName = "0.3.0-monochrome"
+        versionCode = 4
+        versionName = "0.4.0-monochrome"
 
         buildConfigField(
             "String",
